48779b9a05d3b8f0a1613f62db189197b4efe9f8
Deepak Kumar <deepak.kumar.rai@wurth-it.in>
2021-07-22 00:43:37 +0530

    WCB-36 Data-requiremodule bug fixed in theme

---
+++ app/design/frontend/pedona/pedona_default/requirejs-config.js
@@ -0,0 +1,10 @@
+var config = {
+ paths: {
+     fancybox: 'Plazathemes_InstagramGallery::plazathemes/instagramgallery/js/jquery.fancybox.js'
+ },
+ shim: {
+     fancybox: {
+         deps: ['jquery']
+     }
+   }
+};
