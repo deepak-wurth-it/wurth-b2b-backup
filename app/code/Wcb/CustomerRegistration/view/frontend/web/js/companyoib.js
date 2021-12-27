534727f2522af4810d7bf929ee6b966ead77fa8c
Saravanan Duraisamy <saravanan.duraisamy@wuerth-it.com>
2021-12-27 10:52:02 +0530

    Added Customer Registration Page files

--- app/code/Wcb/CustomerRegistration/view/frontend/web/js/companyoib.js
+++ app/code/Wcb/CustomerRegistration/view/frontend/web/js/companyoib.js
@@ -13,27 +13,28 @@ define([
         $('#vat_tax_id, #customer_code').on('change keyup', function () {
             var tax_value = $("#vat_tax_id").val();
             var customer_code = $("#customer_code").val();
-            if (tax_value.length >= minlength && tax_value != ''  && customer_code != '' ) {
+            if (tax_value.length >= minlength && tax_value != ''  && tax_value != '' && customer_code != '' ) {
                 $.ajax({
                     url: AjaxUrl,
                     type: "GET",
                     data: {CompanyOib:tax_value, customer_code:customer_code},
-                    showLoader: true //use for display loader 
-                }).done(function (data) {
-                    if (data.success != null) {
-                        $('#confirm-reg').prop('disabled', false);
-                        $("#company_name").val(data.html);
-                                      
-                        if (data.compid == data.cid) { 
-                            $("#confirm_customer_code").val(customer_code);
-                        }
-                    }                
-                    if (data.success == 'false') { 
-                        $('#confirm-reg').prop('disabled', true);
-                        $("#company_name").val('');
-                        $('#link-error').html("Cusomer is not linked. Please enter valid customer code");  // set the message as html of span
-                    } 
-                    return true;
+                    showLoader: true, //use for display loader,
+                    success: function (data) {
+                        if (data.success != null) {
+                            $('#confirm-reg').prop('disabled', false);
+                            $("#company_name").val(data.html);
+                                          
+                            if (data.compid == data.cid) { 
+                                $("#confirm_customer_code").val(customer_code);
+                            }
+                        }                
+                        if (data.success == 'false') { alert('ghfhf');
+                            $('#confirm-reg').prop('disabled', true);
+                            $("#company_name").val('');
+                            $( "#link-error" ).append( "<p>Cusomer is not linked. Please enter valid customer code</p>" );  // set the message as html of span
+                        } 
+                        return true;
+                    }
                 });}
             });
         });
