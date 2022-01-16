2bb69ca3ee80765e2800557ff7580fa0325b4503
deepak.kumar.rai@wurth-it.in <deepak.kumar.rai@wuerth-it.com>
2022-01-17 02:35:48 +0530

    PLP price and stock Soap API

--- app/design/frontend/wc/b2b_eshop/web/js/plp.js
+++ app/design/frontend/wc/b2b_eshop/web/js/plp.js
@@ -1,71 +1,70 @@
 define([
-  "jquery",
-  "mage/url"
-], 
-function($,urlBuilder) {
-  "use strict";
-  return function(config) {
-  //console.log(config);
-  var GetMultiProductPriceUrl = urlBuilder.build('/wcbcatalog/ajax/GetMultiProductPrice');
-  var GetMultiProductStockUrl = urlBuilder.build('/wcbcatalog/ajax/GetMultiProductStock');
-
-  $.ajax({
-               
-                type: "POST",
-                url: GetMultiProductPriceUrl,
-                data: {skus:config.listPids},
-                cache: false,
-                async: false,
-                success: function(result)
-                {   // $('#price-container #price').html(result);                        
-                   // alert();
-                    //console.log(JSON.parse(result.success));
-
-
-                    if (result) {
-                        var i;
-                        var itemNo,price;
-                        for (i = 0; i < result.length; ++i) {
-                            console.log(result[i]);
-                            itemNo = result[i]['Item No'];
-                            price = result[i]['Suggested Price'];
-                            if(itemNo && price){
-                             $('#price_'.itemNo).html(price);
-                            }
-                        }
-                       
-                    } else {
-                       
-                    }
-                }
-            });
-            
-            
-            $.ajax({
-                type: "POST",
-                url: GetMultiProductStockUrl,
-                data: {skus:config.listPids},
-                cache: false,
-                async: false,
-                success: function(result)
-                {    //$('#price-container #price').html(result);
-                    if (result) {
-                        var i;
-                        var itemNo,quantity;
-                        for (i = 0; i < result.length; ++i) {
-                            console.log(result[i]);
-                            itemNo = result[i]['Item No'];
-                            quantity = result[i]['Available Quantity'];
-                            if(itemNo && quantity){
-                             $('#stock_'.itemNo).html(quantity);
-                            }
-                        }
-                        
-                    } else {
-                       
-                    }
-                }
-            });
-        };
-
-});
+    "jquery",
+    "mage/url"
+  ], 
+  function($,urlBuilder) {
+    "use strict";
+    return function(config) {
+    //console.log(config);
+    var GetMultiProductPriceUrl = urlBuilder.build('/wcbcatalog/ajax/GetMultiProductPrice');
+    var GetMultiProductStockUrl = urlBuilder.build('/wcbcatalog/ajax/GetMultiProductStock');
+  
+    $.ajax({
+                 
+                  type: "POST",
+                  url: GetMultiProductPriceUrl,
+                  data: {skus:config.listPids},
+                  cache: false,
+                  async: false,
+                  success: function(result)
+                  {  
+                      if (result.success) {
+                          var i;
+                          var itemNo,price;
+                          var finalResult = result.success;
+                          for (i = 0; i <= finalResult.length; i++) {
+                              console.log(finalResult);
+                              itemNo = finalResult[i]['Item No'];
+                              price = finalResult[i]['Suggested Price'];
+                              if(itemNo && price){
+                               $('#price_'.itemNo).html(price);
+                              }
+                          }
+                         
+                      } else {
+                         
+                      }
+                  }
+              });
+              
+              
+              $.ajax({
+                  type: "POST",
+                  url: GetMultiProductStockUrl,
+                  data: {skus:config.listPids},
+                  cache: false,
+                  async: false,
+                  success: function(result)
+                  {    
+                      if (result.success) {
+                          var i;
+                          var itemNo,quantity;
+                          var finalResult = result.success;
+                          for (i = 0; i <= finalResult.length; i++) {
+                              console.log(finalResult);
+                              itemNo = finalResult[i]['Item No'];
+                              quantity = finalResult[i]['Available Quantity'];
+                              if(itemNo && quantity){
+                               $('#stock_'.itemNo).html(quantity);
+                              }
+                          }
+                          
+                      } else {
+                         
+                      }
+                  }
+              });
+          };
+  
+  });
+  
