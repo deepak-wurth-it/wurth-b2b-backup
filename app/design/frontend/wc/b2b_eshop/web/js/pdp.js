36062691efc5284f3df84699bb78593e7ec628c7
deepak.kumar.rai@wurth-it.in <deepak.kumar.rai@wuerth-it.com>
2022-02-10 00:01:57 +0530

    Soap Price on PDP

--- app/design/frontend/wc/b2b_eshop/web/js/pdp.js
+++ app/design/frontend/wc/b2b_eshop/web/js/pdp.js
@@ -2,68 +2,71 @@ define([
         "jquery",
         "mage/url"
     ],
-
-    function ($) {
-        "use strict";
-
-        return function qtyincrement() {
-
-            $(document).ready(function() {  
-                var incrementPlus;
-                var incrementMinus;
-                var buttonPlus  = $(".increaseQty");
-                var buttonMinus = $(".decreaseQty");
-                var incrementPlus = buttonPlus.click(function() {
-                    var $n = $(".qty")
-                    $n.val(Number($n.val())+1 );
-                });
-                
-                var incrementMinus = buttonMinus.click(function() {
-                        var $n = $(".qty")
-                    var amount = Number($n.val());
-                    if (amount > 1) {
-                        $n.val(amount-1);
-                    }
-                });
-                
-                });
-
-           
-        }
-    },
-
     function ($, urlBuilder) {
-        "use strict";        
-        return function (config) {
-            //console.log(config);
-            var GetItemEShopSalesPriceAndDisc = urlBuilder.build('/wcbcatalog/ajax/GetItemEShopSalesPriceAndDisc');
-
-            $.ajax({
-
-                type: "POST",
-                url: GetItemEShopSalesPriceAndDisc,
-                data: {
-                    skus: config.pid
-                },
-                cache: false,
-                async: false,
-                success: function (result) {
-                    if (result.success) {
-                        var finalResult = result.success;
-                        if(finalResult.suggestedPriceAsTxtP){
-                          $('#price_pdp').html(finalResult.suggestedPriceAsTxtP);
-                          $("#price_format").css({ display: "block" });
+        "use strict";
+        
+        return {
+            soapPrice: function(productCode) {
+                var GetItemEShopSalesPriceAndDisc = urlBuilder.build('/wcbcatalog/ajax/GetItemEShopSalesPriceAndDisc');
+                //alert();
+                $.ajax({
+        
+                    type: "POST",
+                    url: GetItemEShopSalesPriceAndDisc,
+                    data: {
+                        skus: productCode
+                    },
+                    cache: false,
+                    async: false,
+                    success: function(result) {
+                        if (result.success) {
+                            var finalResult = result.success;
+        
+                            if (finalResult.suggestedPriceAsTxtP) {
+                                $('#suggestedPriceAsTxtP').html(finalResult.suggestedPriceAsTxtP);
+                                $('#suggestedDiscountAsTxtP').html(finalResult.suggestedDiscountAsTxtP);
+                                $('#suggestedSalesPriceInclDiscAsTxtP').html(finalResult.suggestedSalesPriceInclDiscAsTxtP);
+                                $("#price_soap").css({
+                                    display: "block"
+                                });
+                                $("#price_loader").css({
+                                    display: "none"
+                                });
+                            }
+        
+                            console.log(result.success);
+                        } else {
+        
                         }
-
-                        console.log(result.success);
-                    } else {
-
                     }
-                }
-            });
-
-            
-
+                });
+        
+            },
+        
+            qtyincrement: function() {
+                //alert();
+                $(document).ready(function() {
+                    var incrementPlus;
+                    var incrementMinus;
+                    var buttonPlus = $(".increaseQty");
+                    var buttonMinus = $(".decreaseQty");
+                    var incrementPlus = buttonPlus.click(function() {
+                        var $n = $(".qty")
+                        $n.val(Number($n.val()) + 1);
+                    });
+        
+                    var incrementMinus = buttonMinus.click(function() {
+                        var $n = $(".qty")
+                        var amount = Number($n.val());
+                        if (amount > 1) {
+                            $n.val(amount - 1);
+                        }
+                    });
+        
+                });
+            }
+        
+        
         };
 
     });
