a866ccb9134a86723cda5ea3814e6c868278996c
deepak.kumar.rai@wuerth-it.com <deepak.kumar.rai@wuerth-it.com>
2022-03-22 20:27:12 +0530

    Store Pickup

--- app/code/Wcb/Store/view/frontend/web/js/wcbstore.js
+++ app/code/Wcb/Store/view/frontend/web/js/wcbstore.js
@@ -1,56 +1,43 @@
-define(['jquery', 'uiComponent', 'ko'], function ($, Component, ko) {
+define(['jquery', 'uiComponent', 'ko','mage/url'], function ($, Component, ko,url) {
     'use strict';
     var self;
     return Component.extend({
-        myTimer: ko.observable(0),
-        red: ko.observable(0),
-        blue: ko.observable(0),
-        green: ko.observable(0),
         storeOption : ko.observable(0),
         storeChange : ko.observable(0),
+        selectedStore:ko.observable(0),
+        image:ko.observable(0),
+        address:ko.observable(0),
+        phone:ko.observable(0),
+        store_name:ko.observable(0),
+        map_url:ko.observable(0),
+        email:ko.observable(0),
         initialize: function () {
             self = this;
             this._super();
-            //call the incrementTime function to run on intialize
-            this.incrementTime();
-            console.log(this.storeChange());
-            this.subscribeToTime();
             this.storeOption(self.stores_option);
-            this.randomColour = ko.computed(function() {
-                //return the random colour value
-                return 'rgb(' + this.red() + ', ' + this.blue() + ', ' + this.green() + ')';
-            }, this);
-        },
-        //increment myTimer every second
-        incrementTime: function() {
-            var t = 0;
-            setInterval(function() {
-                t++;
-                self.myTimer(t);
-            }, 1000);
-        },
-        subscribeToTime: function() {
-            this.myTimer.subscribe(function(newValue) {
-                console.log(newValue);
-                self.updateTimerTextColour();
-            });
-        },
-        randomNumber: function() {
-            return Math.floor((Math.random() * 255) + 1);
-        },
-        updateTimerTextColour: function() {
-            //define RGB values
-            /*notice we now no longer have to set and return the RBG style code here
-             we simply update the red/blue/green observables and the computed observable
-             returns the style element to the template */
-            this.red(self.randomNumber());
-            this.blue(self.randomNumber());
-            this.green(self.randomNumber());
+            $('#select-delivery input.checkbox').on('change', function() {
+                $('#select-delivery input.checkbox').not(this).prop('checked', false);  
+            }); 
         },
         storeChange: function(value) {
-           console.log(value);
-           console.log(this);
-        }
-
+            //console.log(value);
+            //console.log(self.stores_option);
+            $.each(self.stores_option, function(key,val) {   
+                if(val['entity_id'] == value){
+                    var media = url.build('media');
+                    var imgUrl = media+val['image'];
+                    self.image(imgUrl);
+                    self.store_name(val['name']);
+                    self.address(val['address']);
+                    self.phone(val['phone']);
+                    self.email(val['contact_email']);
+                    self.map_url(val['map_url']);
+                    console.log(val);
+                }           
+            });
+            // this.selectedStore.subscribe(function(newValue) {
+            //     console.log(newValue);
+            // });
+         }
     });
 });
