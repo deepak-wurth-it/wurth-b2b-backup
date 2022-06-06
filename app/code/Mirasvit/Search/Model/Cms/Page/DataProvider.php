50038ecdfba40fadee48c8f23d9fb7cb953ff62f
Sagar Dobariya <sagar.dobariya@wuerth-it.com>
2022-06-06 18:43:24 +0530

    Provide image upload in cms page and some changes in search

---
+++ app/code/Mirasvit/Search/Model/Cms/Page/DataProvider.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Mirasvit\Search\Model\Cms\Page;
+
+use Magento\Cms\Model\ResourceModel\Page\CollectionFactory;
+use Magento\Framework\App\Request\DataPersistorInterface;
+use Magento\Framework\UrlInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Ui\DataProvider\AbstractDataProvider;
+use Mirasvit\Search\Model\ImageUploader;
+
+class DataProvider extends AbstractDataProvider
+{
+    /**
+     * @var Magento\Cms\Model\ResourceModel\Page\CollectionFactory
+     */
+    public $collection;
+    private $loadedData;
+    /**
+     * @var DataPersistorInterface
+     */
+    private $dataPersistor;
+
+    /**
+     * Constructor
+     *
+     * @param string $name
+     * @param string $primaryFieldName
+     * @param string $requestFieldName
+     * @param CollectionFactory $collectionFactory
+     * @param DataPersistorInterface $dataPersistor
+     * @param StoreManagerInterface $storeManager
+     * @param array $meta
+     * @param array $data
+     */
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        DataPersistorInterface $dataPersistor,
+        StoreManagerInterface $storeManager,
+        array $meta = [],
+        array $data = []
+    ) {
+        $this->collection = $collectionFactory->create();
+        $this->dataPersistor = $dataPersistor;
+        $this->storeManager = $storeManager;
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data);
+    }
+
+    /**
+     * Get data
+     *
+     * @return array
+     */
+    public function getData()
+    {
+        if (isset($this->loadedData)) {
+            return $this->loadedData;
+        }
+
+        $items = $this->collection->getItems();
+        foreach ($items as $model) {
+            $this->loadedData[$model->getId()] = $model->getData();
+            // echo $model->getCmsImage();exit();
+            if ($model->getCmsImage()) {
+                $m['cms_image'][0]['name'] = $model->getCmsImage();
+                $m['cms_image'][0]['url'] = $this->getMediaUrl() . $model->getCmsImage();
+                $fullData = $this->loadedData;
+                $this->loadedData[$model->getId()] = array_merge($fullData[$model->getId()], $m);
+            }
+        }
+
+        $data = $this->dataPersistor->get('cms_page');
+
+        if (!empty($data)) {
+            $model = $this->collection->getNewEmptyItem();
+            $model->setData($data);
+            $this->loadedData[$model->getId()] = $model->getData();
+            $this->dataPersistor->clear('cms_page');
+        }
+
+        return $this->loadedData;
+    }
+
+    public function getMediaUrl()
+    {
+        return $this->storeManager->getStore()
+                ->getBaseUrl(UrlInterface::URL_TYPE_MEDIA) . ImageUploader::IMAGE_TMP_PATH . '/';
+    }
+}
