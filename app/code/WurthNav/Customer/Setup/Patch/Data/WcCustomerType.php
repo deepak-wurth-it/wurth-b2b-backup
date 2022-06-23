6b4d5424cea9eaec8fd2ea2debe40b65c26b01d2
deepak.kumar.rai@wuerth-it.com <deepak.kumar.rai@wuerth-it.com>
2022-06-24 00:24:39 +0530

    WCB Sales and Shipping

---
+++ app/code/WurthNav/Customer/Setup/Patch/Data/WcCustomerType.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Copyright ©  All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+
+namespace WurthNav\Customer\Setup\Patch\Data;
+
+use Magento\Framework\Setup\Patch\DataPatchInterface;
+use Magento\Framework\Setup\Patch\PatchRevertableInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Customer\Setup\CustomerSetupFactory;
+use Magento\Customer\Setup\CustomerSetup;
+
+class WcCustomerType implements DataPatchInterface, PatchRevertableInterface
+{
+
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+    /**
+     * @var CustomerSetup
+     */
+    private $customerSetupFactory;
+
+    /**
+     * Constructor
+     *
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CustomerSetupFactory $customerSetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CustomerSetupFactory $customerSetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->customerSetupFactory = $customerSetupFactory;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function apply()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->customerSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->addAttribute(
+            \Magento\Customer\Model\Customer::ENTITY,
+            'wc_customer_type',
+            [
+                'type' => 'varchar',
+                'label' => 'Customer Type',
+                'input' => 'text',
+                'source' => '',
+                'required' => false,
+                'visible' => true,
+                'position' => 500,
+                'system' => false,
+                'backend' => ''
+            ]
+        );
+
+        $attribute = $customerSetup->getEavConfig()->getAttribute('customer', 'wc_customer_type')->addData([
+            'used_in_forms' => [
+                'adminhtml_checkout',
+                'adminhtml_customer',
+                'adminhtml_customer_address',
+                'customer_account_edit',
+                'customer_address_edit',
+                'customer_register_address',
+                'customer_account_create'
+            ]
+        ]);
+        $attribute->save();
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    public function revert()
+    {
+        $this->moduleDataSetup->getConnection()->startSetup();
+        /** @var CustomerSetup $customerSetup */
+        $customerSetup = $this->eavSetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $customerSetup->removeAttribute(\Magento\Customer\Model\Customer::ENTITY, 'wc_customer_type');
+
+        $this->moduleDataSetup->getConnection()->endSetup();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function getDependencies()
+    {
+        return [
+
+        ];
+    }
+}
