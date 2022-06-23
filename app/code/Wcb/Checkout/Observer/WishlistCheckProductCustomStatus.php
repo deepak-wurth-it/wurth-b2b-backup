d47f3aedc99d3a2e4022bb7162c4dbf0ee6ce124
Sagar Dobariya <sagar.dobariya@wuerth-it.com>
2022-06-23 18:35:45 +0530

    - Add replacement code in plp, wishlist, cart page

---
+++ app/code/Wcb/Checkout/Observer/WishlistCheckProductCustomStatus.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Wcb\Checkout\Observer;
+
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Checkout\Model\SessionFactory;
+use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Wishlist\Model\ItemFactory;
+use Wcb\Checkout\Helper\ManageProductStatus;
+
+class WishlistCheckProductCustomStatus implements ObserverInterface
+{
+    /**
+     * @var RequestInterface
+     */
+    protected $request;
+    /**
+     * @var SessionFactory
+     */
+    protected $checkoutSession;
+    /**
+     * @var ManagerInterface
+     */
+    protected $messageManager;
+    /**
+     * @var ProductRepositoryInterface
+     */
+    protected $productRepository;
+    /**
+     * @var ManageProductStatus
+     */
+    protected $manageProductStatus;
+    /**
+     * @var ItemFactory
+     */
+    protected $itemFactory;
+
+    /**
+     * CheckProductCustomStatus constructor.
+     * @param RequestInterface $request
+     * @param ManagerInterface $messageManager
+     * @param SessionFactory $checkoutSession
+     * @param ProductRepositoryInterface $productrepositoryInterface
+     * @param ManageProductStatus $manageProductStatus
+     */
+    public function __construct(
+        RequestInterface $request,
+        ManagerInterface $messageManager,
+        SessionFactory $checkoutSession,
+        ProductRepositoryInterface $productrepositoryInterface,
+        ManageProductStatus $manageProductStatus,
+        ItemFactory $itemFactory
+    ) {
+        $this->request = $request;
+        $this->messageManager = $messageManager;
+        $this->checkoutSession = $checkoutSession;
+        $this->manageProductStatus = $manageProductStatus;
+        $this->productRepository = $productrepositoryInterface;
+        $this->itemFactory = $itemFactory;
+    }
+
+    /**
+     * @param Observer $observer
+     * @throws NoSuchEntityException
+     */
+    public function execute(Observer $observer)
+    {
+        $itemId = $observer->getRequest()->getParam('item');
+        $item = $this->itemFactory->create()->load($itemId);
+
+        $productId = $item->getProductId();
+        $qty = $observer->getRequest()->getParam('qty');
+
+        $product = $this->productRepository->getById($productId);
+        $result = $this->manageProductStatus->checkDiscontinuedProductStatus($product, $qty);
+        if (!$result['allow_add_to_cart']) {
+            $observer->getRequest()->setParam('item', false);
+        }
+    }
+}
