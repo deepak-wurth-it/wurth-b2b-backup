268dd3d84973ccefa784151c1d46cee4ccf62369
amitsswn <amit.sharma@wuerth-it.com>
2022-07-01 10:01:06 +0530

    Pushed code for datatable

---
+++ app/design/frontend/wc/b2b_eshop/Magento_Sales/requirejs-config.js
@@ -0,0 +1,12 @@
+var config = {
+    paths: {            
+            'jquery.dataTables.min' : 'js/jquery.dataTables.min', 
+        },                                                                
+    shim: {
+    'jquery.dataTables.min': {
+        deps: ['jquery']
+    },
+  }
+};
+
+
