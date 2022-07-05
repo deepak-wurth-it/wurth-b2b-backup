dff56650e68a10659f331db9bf8d257a851f5618
deepak.kumar.rai@wuerth-it.com <deepak.kumar.rai@wuerth-it.com>
2022-07-06 01:47:13 +0530

    Fixed customer block issues,add some blocks undelivered and sales history

---
+++ app/code/Wcb/Sales/Controller/Order/Undelivered.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+namespace Wcb\Sales\Controller\Order;
+
+
+class Undelivered extends \Magento\Framework\App\Action\Action
+{
+	protected $_pageFactory;
+
+	public function __construct(
+		\Magento\Framework\App\Action\Context $context,
+		\Magento\Framework\View\Result\PageFactory $pageFactory)
+	{
+		$this->_pageFactory = $pageFactory;
+		return parent::__construct($context);
+	}
+
+	public function execute()
+	{
+		return $this->_pageFactory->create();
+	}
+}
--- app/design/frontend/wc/b2b_eshop/requirejs-config.js
+++ app/design/frontend/wc/b2b_eshop/requirejs-config.js
@@ -1,23 +1,28 @@
 var config = {
- map: {
-         '*': {
-                 'plp': 'js/plp',
-                 'pdp': 'js/pdp',
-                 'checkoutjs' : 'Magento_Checkout/js/chekout',
-                 'qtyIncrementWidget':'js/pdp-widget',
-                 'plpJsWidget':'js/plp-widget',
-             }
-         },
-paths: {
-     fancybox: 'Plazathemes_InstagramGallery::plazathemes/instagramgallery/js/jquery.fancybox.js'
- },
-shim: {
-     fancybox: {
-         deps: ['jquery']
-     }
-   },
-   
-deps: ['js/app']   
+    map: {
+        '*': {
+            'plp': 'js/plp',
+            'pdp': 'js/pdp',
+            'checkoutjs': 'Magento_Checkout/js/chekout',
+            'qtyIncrementWidget': 'js/pdp-widget',
+            'plpJsWidget': 'js/plp-widget',
+            'dataTables': 'js/jquery.dataTables.min',
+
+        }
+    },
+    paths: {
+        fancybox: 'Plazathemes_InstagramGallery::plazathemes/instagramgallery/js/jquery.fancybox.js'
+    },
+    shim: {
+        fancybox: {
+            deps: ['jquery']
+        },
+        dataTables: {
+            deps: ['jquery']
+        },
+    },
+
+    deps: ['js/app']
 };
 
- 
+
